44769a993348400df4b5ae3a8846f86c5e2f4be3
Pereira <diego.p.pereira@accenture.com>
2024-02-18 17:06:27 -0300

    ajuste de cors

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require("cors");
 
 const app = express()
 app.use(express.json())
+app.use(cors());
 
 app.use('/', 
     require('./route/tests-route.js')
